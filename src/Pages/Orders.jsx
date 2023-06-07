81ea141e227a3cbfba18a2a747c337107b145be1
richard9809 <richard.ndisya@strathmore.edu>
2023-06-07 22:24:44 +0300

    CH9
    Signed-off-by: richard9809 <richard.ndisya@strathmore.edu>

--- src/Pages/Orders.jsx
+++ src/Pages/Orders.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import OrdersTable from '../Component/OrdersTable'
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div className='orders-page px-8 py-2 '>
+      <h1 className='font-semibold text-2xl '>Orders Table</h1>
+      <div className='py-2'>
+        <OrdersTable />
+      </div>
+    </div>
   )
 }
 
